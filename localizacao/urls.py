19ce166f95aa91b744f6598f59cea9f4afadd723
Louremberg <kaua.lima@layoutsistemas.com.br>
2025-12-01 00:56:03 -0300

    correcao

--- localizacao/urls.py
+++ localizacao/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from localizacao.views import LocalizacaoView, AreaSeguraListCreateView, LocalizacaoAmparadoView, MarcadoresView, \
-    MarcadorDetailView
+    MarcadorDetailView, AreaSeguraDetailView
 
 urlpatterns = [
     path('localizacao/', LocalizacaoView.as_view(), name='localizacao'),
     path('areas/', AreaSeguraListCreateView.as_view(), name='areas'),
-    path('areas/<int:id>/', AreaSeguraListCreateView.as_view(), name='areas'),
+    path('areas/<int:id>/', AreaSeguraDetailView.as_view(), name='areas'),
     path('geofencing/', LocalizacaoAmparadoView.as_view(), name='geofencing'),
     path('marcadores/', MarcadoresView.as_view(), name='marcadores'),
     path('marcadores/<int:id>/', MarcadorDetailView.as_view(), name='marcador-detail'),
