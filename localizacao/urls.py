13b58e7dbb09cd5dee0733b168d0d160a262ba94
Kauã Louremberg <161601940+KauaLouremberg@users.noreply.github.com>
2025-12-01 17:47:04 -0300

    Atualizar o urls.py

--- localizacao/urls.py
+++ localizacao/urls.py
@@ -6,7 +6,7 @@ from localizacao.views import LocalizacaoView, AreaSeguraListCreateView, Localiz
 urlpatterns = [
     path('localizacao/', LocalizacaoView.as_view(), name='localizacao'),
     path('areas/', AreaSeguraListCreateView.as_view(), name='areas'),
-    path('areas/<int:id>/', AreaSeguraDetailView.as_view(), name='areas'),
+    path('areas/<int:id>/', AreaSeguraDetailView.as_view(), name='areas-detail'),
     path('geofencing/', LocalizacaoAmparadoView.as_view(), name='geofencing'),
     path('marcadores/', MarcadoresView.as_view(), name='marcadores'),
     path('marcadores/<int:id>/', MarcadorDetailView.as_view(), name='marcador-detail'),
